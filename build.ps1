bb00f0344560e8bc09a35dd70102f3e6b4b8d129
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-11-06 15:31:44 +0100

    remove GLUT dependency since it is not used anymore

--- build.ps1
+++ build.ps1
@@ -7,6 +7,8 @@ cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scri
 #statically link is broken on Windows due to freeglut library having the wrong name (freeglut_static.lib instead of just freeglut.lib) as of August 2017 - rename manually it if you want to build a static executable
 #cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static"
 
+#cmake --build . --config Debug
 cmake --build . --config Release
+
 cd ..
 
