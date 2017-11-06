ab1355a545a25ce4ae9cee56dafad3969d7d9e4a
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-11-06 12:24:26 +0100

    impossible to use install target with dynamic linking and vcpkg

--- build.ps1
+++ build.ps1
@@ -7,6 +7,6 @@ cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scri
 #static link is broken on Windows due to freeglut library having the wrong name (freeglut_static.lib instead of just freeglut.lib) as of August 2017
 #cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static"
 
-cmake --build . --target install --config Release
+cmake --build . --config Release
 cd ..
 
