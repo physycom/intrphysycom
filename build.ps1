1fb7f14c551a31528dd6a7b81f13e0f7bf8a2ab1
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-11-09 15:47:04 +0100

    use static libraries on windows, fixes many FLTK bugs with dynamics

--- build.ps1
+++ build.ps1
@@ -4,12 +4,11 @@ mkdir -Force build
 cd build
 
 #dynamically linked
-#cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake"
+#cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DCMAKE_BUILD_TYPE=Release"
 
 #statically linked
-cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static"
+cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static" "-DCMAKE_BUILD_TYPE=Release"
 
-cmake --build . --config Debug
-#cmake --build . --config Release
+cmake --build . --config Release
 
 cd ..
