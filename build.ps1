3165cdb924b3d50488195cd011500516e73be13a
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-11-25 08:56:07 +0100

    remove dynamic building option until is broken

--- build.ps1
+++ build.ps1
@@ -3,10 +3,8 @@
 mkdir -Force build
 cd build
 
-#dynamically linked
-#cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$env:WORKSPACE\sysconfig\cmake\physycom_toolchain.cmake" -DCMAKE_BUILD_TYPE=Release"
-
-#statically linked
+#FLTK are broken as of november 2017 if you use dynamic linking
+#So we have to use a static triplet for vcpkg
 cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$env:WORKSPACE\sysconfig\cmake\physycom_toolchain.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static" "-DCMAKE_BUILD_TYPE=Release"
 
 cmake --build . --config Release
