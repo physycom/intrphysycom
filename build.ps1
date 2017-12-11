542352e6e60a7ed9f90af83ccd4ce2a459bd155c
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-12-11 11:26:15 +0100

    use conventional order in cmake commands, explicitly set build type

--- build.ps1
+++ build.ps1
@@ -4,9 +4,7 @@ mkdir -Force build
 cd build
 
 #FLTK are broken as of november 2017 if you use dynamic linking
-#So we have to use a static triplet for vcpkg
-cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$env:WORKSPACE\sysconfig\cmake\physycom_toolchain.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static" "-DCMAKE_BUILD_TYPE=Release"
-
+cmake -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$env:WORKSPACE\sysconfig\cmake\physycom_toolchain.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static" "-DCMAKE_BUILD_TYPE=Release" ..
 cmake --build . --config Release
 
 cd ..
--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p build
 cd build
-cmake .. "-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=${WORKSPACE}\sysconfig\cmake\physycom_toolchain.cmake" 
-#cmake .. -DINTERNET_DISABLED:BOOL=TRUE "-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=${WORKSPACE}\sysconfig\cmake\physycom_toolchain.cmake" 
+cmake "-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=${WORKSPACE}\sysconfig\cmake\physycom_toolchain.cmake" ..
+#cmake -DINTERNET_DISABLED:BOOL=TRUE "-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=${WORKSPACE}\sysconfig\cmake\physycom_toolchain.cmake" ..
 cmake --build . 
 cd ..
