1d95c6cfed06ff43f89888f2be57b477909ad799
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-02-01 00:16:07 +0100

    ease cmake setup

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ mkdir -Force build
 cd build
 
 #FLTK are broken as of november 2017 if you use dynamic linking
-cmake -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$env:WORKSPACE\sysconfig\cmake\physycom_toolchain.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static" "-DCMAKE_BUILD_TYPE=Release" ..
-cmake --build . --config Release
+cmake -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static" "-DCMAKE_BUILD_TYPE=Release" ..
+cmake --build . --config Release --target install
 
 cd ..
